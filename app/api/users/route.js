42d2f280bdf0baaf434548dade4ed9a167e0026f
PRABHANSHUBHOWAL <prabhanshubhowal@gmail.com>
2024-03-29 16:25:36 +0530

    add next tuttorial files

---
+++ app/api/users/route.js
@@ -0,0 +1,18 @@
+export async function GET(request) {
+  return new Response("Hello Next.js");
+}
+export async function HEAD(request) {
+  return new Response("Hello Next.js");
+}
+export async function POST(request) {
+  return new Response("Hello Next.js");
+}
+export async function PUT(request) {
+  return new Response("Hello Next.js");
+}
+export async function DELETE(request) {
+  return new Response("Hello Next.js");
+}
+export async function PATCH(request) {
+  return new Response("Hello Next.js");
+}
